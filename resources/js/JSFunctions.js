1f5964034350ea5235e5cc3c89c55a43ff75850f
Jeremy Seth Gubman <jeremygubman@u.northwestern.edu>
2017-11-06 12:48:10 -0600

    revert lol

--- resources/js/JSFunctions.js
+++ resources/js/JSFunctions.js
@@ -2,3 +2,21 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover({ trigger: "hover" });
 });
 
+
+ $(document).ready(function () {
+     $('.canvas_icon').click(function () {
+     	if ($('#sidebar').hasClass('active')){
+     		$('#sidebar').removeClass('active');
+        	$('.canvas_icon').addClass('active');
+     	} 
+     });
+ });
+
+$(document).ready(function () {
+    $('#map').on('click', function(){
+		if ($('#sidebar').hasClass('active') == false) {
+     		$('#sidebar').addClass('active');
+         	$('.canvas_icon').removeClass('active');
+     	}
+     })
+ });
