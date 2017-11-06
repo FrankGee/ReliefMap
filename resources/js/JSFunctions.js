808d78e8fd140f67dcfd2a92c3414b0cb9d994bd
Jeremy Seth Gubman <jeremygubman@u.northwestern.edu>
2017-11-06 11:17:40 -0600

    slider

--- resources/js/JSFunctions.js
+++ resources/js/JSFunctions.js
@@ -2,3 +2,10 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover({ trigger: "hover" });
 });
 
+
+ $(document).ready(function () {
+     $('.canvas_icon').on('click', function () {
+         $('#sidebar').toggleClass('active');
+         $('.canvas_icon').toggleClass('active');
+     });
+ });
