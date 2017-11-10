5f1d5169f61b3a5221c001c94b027f0072f509f0
macchiatoyn <shinyyn@gmail.com>
2017-11-10 12:38:05 -0600

    Update filter

--- resources/js/JSFunctions.js
+++ resources/js/JSFunctions.js
@@ -20,3 +20,70 @@ $(document).ready(function () {
      	}
      })
  });
+
+$(document).ready(function () {
+    $("#food_icons").on("click", function() {
+        $("#icon_first_aid1").toggle(false);
+        $("#icon_first_aid2").toggle(false);
+        $("#icon_shop").toggle(false);
+        $("#icon_police").toggle(false);
+
+        $("#icon_food1").toggle(true);
+        $("#icon_food2").toggle(true);
+        $("#icon_food3").toggle(true);
+        $("#icon_food4").toggle(true);
+    });
+
+    $("#medication_icons").on("click", function() {
+        $("#icon_shop").toggle(false);
+        $("#icon_police").toggle(false);
+        $("#icon_food1").toggle(false);
+        $("#icon_food2").toggle(false);
+        $("#icon_food3").toggle(false);
+        $("#icon_food4").toggle(false);
+
+        $("#icon_first_aid1").toggle(true);
+        $("#icon_first_aid2").toggle(true);
+    });
+
+    $("#supply_icons").on("click", function() {
+        $("#icon_first_aid1").toggle(false);
+        $("#icon_first_aid2").toggle(false);
+        $("#icon_police").toggle(false);
+        $("#icon_food1").toggle(false);
+        $("#icon_food2").toggle(false);
+        $("#icon_food3").toggle(false);
+        $("#icon_food4").toggle(false);
+
+        $("#icon_shop").toggle(true);
+
+
+    });
+
+    $("#police_icons").on("click", function() {
+        $("#icon_first_aid1").toggle(false);
+        $("#icon_first_aid2").toggle(false);
+        $("#icon_food1").toggle(false);
+        $("#icon_food2").toggle(false);
+        $("#icon_food3").toggle(false);
+        $("#icon_food4").toggle(false);
+        $("#icon_shop").toggle(false);
+
+        $("#icon_police").toggle(true);
+
+    });
+
+
+    $("#all_icons").on("click", function() {
+        $("#icon_first_aid1").toggle(true);
+        $("#icon_first_aid2").toggle(true);
+        $("#icon_food1").toggle(true);
+        $("#icon_food2").toggle(true);
+        $("#icon_food3").toggle(true);
+        $("#icon_food4").toggle(true);
+        $("#icon_shop").toggle(true);
+        $("#icon_police").toggle(true);
+    });
+
+});
+
